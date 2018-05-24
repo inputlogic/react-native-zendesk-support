4ea0bfd92c228cd705ec4211ee69005ba02a137c
trustfall <syn3rgy@live.ca>
2018-05-24 14:59:14 -0600

    modifies color scheme and fonts for goose

--- ios/RNZenDeskSupport.m
+++ ios/RNZenDeskSupport.m
@@ -44,6 +44,30 @@ RCT_EXPORT_METHOD(setupIdentity:(NSDictionary *)identity){
 }
 
 RCT_EXPORT_METHOD(showHelpCenterWithOptions:(NSDictionary *)options) {
+    //Setup navbar
+    NSDictionary *navbarAttributes = [NSDictionary dictionaryWithObjectsAndKeys:
+                                      [UIColor whiteColor] ,UITextAttributeTextColor, nil];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0.97 green:0.15 blue:0.59 alpha:1.0];];
+    [[UINavigationBar appearance] setTitleTextAttributes:navbarAttributes];
+    [[UISearchBar appearance] setBarTintColor:[UIColor colorWithRed:208.0f/255.0f green:218.0f/255.0f blue:224.0f/255.0f alpha:1.0f]];
+    //Style Support SDK
+    ZDKTheme * theme = [ZDKTheme baseTheme];
+    
+    theme.fontName = @"Graphik";
+    theme.boldFontName = @"Graphik-Medium";
+    
+    theme.primaryTextColor = [UIColor colorWithRed:0.08 green:0.09 blue:0.17 alpha:1.0];;
+    theme.secondaryTextColor = [UIColor colorWithRed:0.36 green:0.37 blue:0.47 alpha:1.0];;
+    theme.primaryBackgroundColor = [UIColor colorWithRed:1.00 green:1.00 blue:1.00 alpha:1.0];
+    theme.secondaryBackgroundColor = [UIColor colorWithRed:0.99 green:0.99 blue:0.99 alpha:1.0];
+    theme.emptyBackgroundColor = [UIColor colorWithRed:0.98 green:0.98 blue:0.99 alpha:1.0];
+    theme.metaTextColor = [UIColor colorWithRed:139.0f/255.0f green:139.0f/255.0f blue:150.0f/255.0f alpha:1.0f];
+    theme.separatorColor = [UIColor colorWithRed:237.0f/255.0f green:237.0f/255.0f blue:241.0f/255.0f alpha:1.0f];
+    theme.inputFieldTextColor = [UIColor colorWithRed:47.0f/255.0f green:46.0f/255.0f blue:63.0f/255.0f alpha:1.0f];
+    theme.inputFieldBackgroundColor = [UIColor colorWithRed:255.0f/255.0f green:255.0f/255.0f blue:255.0f/255.0f alpha:1.0f];
+    [theme apply];
+
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow *window=[UIApplication sharedApplication].keyWindow;
         UIViewController *vc = [window rootViewController];
